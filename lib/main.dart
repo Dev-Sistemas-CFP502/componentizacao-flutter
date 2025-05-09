9c44817d6f1c29ac356800faaa4d77d8784db77f
devthigas502 <thiagodev502@gmail.com>
2025-05-08 21:25:29 -0300

    criando custom title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:componentes_flutter/ui/login_screen.dart';
+import 'package:componentes_flutter/ui/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
